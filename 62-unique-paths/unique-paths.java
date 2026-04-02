f640555108eb14e73910cd85f12770c03e087115
nihilthirumoorthy <nihilt53@gmail.com>
2026-04-02 12:10:11 +0530

    Time: 1 ms (13.70%) | Memory: 41.5 MB (99.55%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int num[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0){
+                    num[i][j]=1;
+                }
+                else{
+                    num[i][j]=num[i-1][j]+num[i][j-1];
+                }
+            }
+        }
+        return num[m-1][n-1];
+    }
+}
